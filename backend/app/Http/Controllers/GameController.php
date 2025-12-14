5f1505aa918f626e50390ddbecbc62fac54cb860
Kai Akkermann <kai.akkermann@kighlander.de>
2025-12-15 00:59:11 +0100

    Fix missing Validator import in GameController causing 500 error on game creation

--- backend/app/Http/Controllers/GameController.php
+++ backend/app/Http/Controllers/GameController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 use App\Models\Game;
 use App\Services\RankingService;
 use App\Models\User;
